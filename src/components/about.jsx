6c00e3dcb96e517b52eb2ec371608e2e81ffee61
meankch <me.kukkig@gmail.com>
2022-04-16 19:53:50 +0700

    add contents

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {  } from '../styles/about.css';
+import {} from "../styles/about.css";
 
 export default class About extends Component {
   render() {
@@ -16,7 +16,14 @@ export default class About extends Component {
                   <div className="col-md-12">
                     <div className="about-desc">
                       <h2 className="heading">Who Am I?</h2>
-                      <p>I am a software developer</p>
+                      <p>
+                        I am a Software Developer with a demonstrated history of
+                        more than 3 years working in the information technology
+                        and services industry. Skilled in C# .NET, Java, Apigee
+                        API Management Platform, CI/CD using Jenkins, and Spring
+                        framework. Strong engineering professional with a
+                        Bachelor's degree focused in Computer Engineering.
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -31,47 +38,117 @@ export default class About extends Component {
                 className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
                 data-animate-effect="fadeInLeft"
               >
-                <h2 className="heading">
-                  Here are some of my expertise
-                </h2>
+                <h2 className="heading">Experiences</h2>
               </div>
             </div>
-            <div className="row row-pt-md">
-              <div className="col-md-4 text-center animate-box">
+            <div className="column">
+              <div className="row text-center animate-box shadow-sm experiences">
                 <div className="services color-1">
-                  <span className="icon">
-                    <i className="icon-bulb" />
-                  </span>
+                  <div className="row exp-title">
+                    <div className="col-2">
+                      <img
+                        id="svp-img"
+                        src="../assets/sevenpeaks-logo.jpg"
+                        alt="sevenpeaks-software-logo"
+                      ></img>
+                    </div>
+                    <div className="col-8">
+                      <h3>
+                        Mid-Level Software Developer at Seven Peaks Software
+                      </h3>
+                    </div>
+                  </div>
                   <div className="desc">
-                    <h3>Mid-Level Software Developer at Seven Peaks Software</h3>
-                    <p>
-                      Desgins, develops and maintains the backend server of a Property Managerment platform which serves the users, the professional real estate agencies, of around 100+ per day.
+                    <p className="experiences-paragragh">
+                      • Desgins, develops and maintains the backend server of a
+                      Property Managerment platform which serves the users, the
+                      professional real estate agencies, of around 100+ per day.
+                    </p>
+                    <p className="experiences-paragragh">
+                      • Implemented APIs using C# WebApi to enable both mobile
+                      application and the CRM web application to retrieve data
+                      used within the app from MongoDB, ElasticSearch, and Azure
+                      Blob Storage
+                    </p>
+                    <p className="experiences-paragragh">
+                      • Maintain and develop new features for a customizable
+                      website using Squidex and Angular
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="col-md-4 text-center animate-box">
+              <div className="row text-center animate-box shadow-sm experiences">
                 <div className="services color-3">
-                  <span className="icon">
-                    <i className="icon-phone3" />
-                  </span>
+                  <div className="row exp-title">
+                    <div className="col-2">
+                      <img
+                        id="svp-img"
+                        src="../assets/hydroneo-logo.png"
+                        alt="hydroneo-ltd-logo"
+                      ></img>
+                    </div>
+                    <div className="col-8">
+                      <h3>Software Developer at HydroNeo Ltd,.</h3>
+                    </div>
+                  </div>
                   <div className="desc">
-                    <h3>Software Developer at HydroNeo Ltd,.</h3>
                     <p>
-                      Designs, develops, and maintain both the backend server and the mobile application by using Flutter as a development framework for the mobile app. And used RxJava with Spring Framework for the backend server.
+                      Designs, develops, and maintain both the backend server
+                      and the mobile application by using Flutter as a
+                      development framework for the mobile app. And used RxJava
+                      with Spring Framework for the backend server.
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="col-md-4 text-center animate-box">
+              <div className="row text-center animate-box shadow-sm experiences">
                 <div className="services color-5">
-                  <span className="icon">
-                    <i className="icon-data" />
-                  </span>
+                  <div className="row exp-title">
+                    <div className="col-2">
+                      <img
+                        id="svp-img"
+                        src="../assets/accenture-logo.png"
+                        alt="accenture-corp-logo"
+                      ></img>
+                    </div>
+                    <div className="col-8">
+                      <h3>
+                        Application Development Senior Analyst at Accenture
+                      </h3>
+                    </div>
+                  </div>
                   <div className="desc">
-                    <h3>Application Development Senior Analyst at Accenture</h3>
                     <p>
-                      Involved in design process and also develops & maintain the API Gateway using Apigee for a large-scale mobile-banking application that servers the target user of more than 1 million users per month.
+                      Involved in design process and also develops & maintain
+                      the API Gateway using Apigee for a large-scale
+                      mobile-banking application that servers the target user of
+                      more than 1 million users per month.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="row text-center animate-box shadow-sm experiences">
+                <div className="services color-5">
+                  <div className="row exp-title">
+                    <div className="col-2">
+                      <img
+                        id="svp-img"
+                        src="../assets/accenture-logo.png"
+                        alt="accenture-corp-logo"
+                      ></img>
+                    </div>
+                    <div className="col-8">
+                      <h3>
+                        Application Development Analyst at Accenture
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>
+                      Involved in design process and also develops & maintain
+                      the API Gateway using Apigee for a large-scale
+                      mobile-banking application that servers the target user of
+                      more than 1 million users per month.
                     </p>
                   </div>
                 </div>
