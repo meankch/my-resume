07a64157d4a3ac2e4422be7466f126e4dbab6ca5
meankch <me.kukkig@gmail.com>
2022-04-16 14:20:05 +0700

    add react-bootstrap pack

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
